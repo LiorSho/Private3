7643c48dc2ed9f401b6510b94182870ddf207dfa
LiorSho <76389741+LiorSho@users.noreply.github.com>
2021-08-04 13:57:55 +0300

    Update sdf.py

--- sdf.py
+++ sdf.py
@@ -2,3 +2,8 @@ algolia: {
     apiKey: '09a11b10801874df7d226df4f2ce8e8f',
     indexName: 'shrinerb',
   }
+    
+    algolia: {
+    apiKey: '09a11b10801874df7d226df4f2ce8e8f',
+    indexName: 'shrinerb',
+  }
